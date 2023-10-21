a1d503e702b968b04f510b84daa06fda77fd3f4e
LuizHLG <luizlelek08@gmail.com>
2023-10-21 17:06:14 -0300

    Padronização de ponto e vírgula nos imports completa

--- src/provider/UserContext.jsx
+++ src/provider/UserContext.jsx
@@ -1,7 +1,7 @@
-import { createContext, useEffect, useState } from "react";
-import { api } from "../services/api";
-import { toast } from "react-toastify";
-import { useNavigate } from "react-router-dom";
+import { createContext, useEffect, useState } from "react"
+import { api } from "../services/api"
+import { toast } from "react-toastify"
+import { useNavigate } from "react-router-dom"
 
 export const UserContext = createContext({})
 
